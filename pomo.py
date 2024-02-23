5c14d0f6b516b33a9b4fd733be7a4fd070f188a8
jjwhidby <30755039+jjwhidby@users.noreply.github.com>
2024-02-23 18:08:53 -0500

    Update pomo.py

--- pomo.py
+++ pomo.py
@@ -4,12 +4,12 @@ from tkinter import *
 # Define timer lengths
 pomodoro_time = 25 # minutes
 short_break_time = 5
-long_break_time = 20
+long_break_time = 30
 
 # Global variables
 current_time = 0
 current_status = "Pomodoro"
-reps = 0
+reps = 1
 
 def start_timer():
     global current_time, current_status, reps
@@ -18,32 +18,36 @@ def start_timer():
     elif current_status == "Short Break":
         current_time = short_break_time * 60
     elif current_status == "Long Break":
-        current_time == long_break_time * 60
+        current_time = long_break_time * 60
     
-    reps += 1 
+    
     count_down()
     start_button.config(state="disabled")
 
 def count_down():
-    global current_time, current_status
+    global current_time, current_status, reps
     if current_time > 0:
         current_time -= 1
         time_string = f"{int(current_time / 60):02d}:{int(current_time % 60):02d}"
         timer_label.config(text = time_string)
         timer_label.after(1000, count_down)
     else:
-        if current_status == "Pomodoro":
-            if reps % 4 == 0:
-                current_status = "Long Break"
+        if reps % 3 != 0:                   
+            if current_status == "Pomodoro":
+               current_status = "Short Break"
             else:
-                current_status = "Short Break"
+               current_status = "Pomodoro"
+               reps += 1
         else:
-            current_status = "Pomodoro"
+            current_status = "Long Break"
+            reps += 1
+
+        set_status_label()
         start_button.config(state="normal")
 
-
-
-
+def set_status_label():
+    status_label.config(text=current_status)
+    rep_label.config(text=str(reps))
 
 # Create the main window
 root = Tk()
@@ -53,11 +57,13 @@ root.title("Pomodoro Timer")
 timer_label = Label(root, font=("Arial", 40), text="25:00")
 start_button = Button(root, text="Start", command=start_timer)
 status_label = Label(root, font=('Arial', 25), text=current_status )
+rep_label = Label(root, font=('Arial', 25), text=str(reps) ) # Rep Counter does not work
 
 # Layout the widgets
 timer_label.pack(pady=20)
 start_button.pack(pady=20)
-
+status_label.pack()
+# rep_label.pack() Rep counter does not work
 
 #Run the main loop
 root.mainloop()
